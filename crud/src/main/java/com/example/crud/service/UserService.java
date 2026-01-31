46a97c2d929ee169688e28e573dbe048c4cc9c5f
Isaac Santos <isaacsantos7953@gmail.com>
2026-01-31 01:12:32 -0300

    Update UserService.java

--- crud/src/main/java/com/example/crud/service/UserService.java
+++ crud/src/main/java/com/example/crud/service/UserService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+// esse service ta bem basico
 @Service
 public class UserService {
 
@@ -31,3 +31,4 @@ public class UserService {
 	    }
 	
 }
+
