727473d31287f1414043b332e70f2d31a8fa414d
Isaac Santos <isaacsantos7953@gmail.com>
2026-01-31 01:13:37 -0300

    Update UserController.java

--- crud/src/main/java/com/example/crud/controller/UserController.java
+++ crud/src/main/java/com/example/crud/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+// tudo ok por aqui, somente esse /api/users que achei meio desnecessario. poderia ser somente api ou users
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -49,3 +49,4 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 }
+
